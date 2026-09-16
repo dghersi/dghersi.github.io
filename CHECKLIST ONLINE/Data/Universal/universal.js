6b28e6de1b929c0fcd08be2e26925f035f3ee215
dghersi <86135947+dghersi@users.noreply.github.com>
2026-09-16 02:24:25 -0500

    agregar

---
+++ CHECKLIST ONLINE/Data/Universal/universal.js
@@ -0,0 +1,34 @@
+// Data Universal: Inspecciones Transversales y Opciones Decisoras de Hallazgos
+const universalChecklist = {
+  extintor: [
+    { id: "ext_1", t: "Tipo / Capacidad: PQS clase ABC (6 lbs / 9 lbs)", c: "ALTO" },
+    { id: "ext_2", t: "Manómetro: Indicador dentro del rango verde operativo", c: "ALTO" },
+    { id: "ext_3", t: "Vigencia: Tarjeta de inspección y recarga vigente", c: "ALTO" },
+    { id: "ext_4", t: "Precinto: Precinto de seguridad e pasador intactos", c: "MEDIO" },
+    { id: "ext_5", t: "Fijación: Soporte fijo, señalizado y de fácil acceso", c: "MEDIO" }
+  ],
+  botiquin: [
+    { id: "bot_1", t: "Antisépticos: Alcohol, agua oxigenada, jabón antiséptico (limpios)", c: "MEDIO" },
+    { id: "bot_2", t: "Curación: Gasas, esparadrapo, vendas, algodón (sin contaminar)", c: "MEDIO" },
+    { id: "bot_3", t: "Instrumental: Tijeras de punta roma, guantes de examen", c: "MEDIO" },
+    { id: "bot_4", t: "Medicamentos: Paracetamol, antiinflamatorio tópico", c: "MEDIO" },
+    { id: "bot_5", t: "Otros: Linterna, manta térmica, instructivo de primeros auxilios", c: "MEDIO" }
+  ],
+  opcionesBloqueo: [
+    "Digital + Tarjeta Física en Switch Master",
+    "Digital + Llave Retirada y Entregada a SST",
+    "Solo Bloqueo Digital en Sistema",
+    "No Requiere Bloqueo (Falla Menor)"
+  ],
+  estadosNoConforme: [
+    "EQUIPO NO OPERATIVO (BLOQUEO LOTO)",
+    "EQUIPO OPERATIVO CONOBSERVACIONES MENORES",
+    "EQUIPO APTO Y OPERATIVO"
+  ]
+};
+
+// Data/Universal/universal.js - Recursos Compartidos
+export const UNIVERSAL_ASSETS = {
+  extintorUrl: "https://res.cloudinary.com/ovirred3/image/upload/Checklists_PWA/Assets/Universal/extintor.jpg",
+  botiquinUrl: "https://res.cloudinary.com/ovirred3/image/upload/Checklists_PWA/Assets/Universal/botiquin.jpg"
+};
